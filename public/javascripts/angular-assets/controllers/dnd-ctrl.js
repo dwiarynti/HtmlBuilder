110ffa457250703d584fe26c4e8016bcfeb6cd13
Rjuzariah <riajuzariah@gmail.com>
2017-05-17 14:11:01 +0700

    update

--- public/javascripts/angular-assets/controllers/dnd-ctrl.js
+++ public/javascripts/angular-assets/controllers/dnd-ctrl.js
@@ -1,4 +1,13 @@
 app.controller('dndcontroller', function ($scope, $filter, $rootScope,$state) {
-    $scope.panel = ["a","b","c","d"];
+    $scope.panel = [
+        {"value":"a", "color":"red"},
+        {"value":"b", "color":"blue"},
+        {"value":"c", "color":"green"},
+        {"value":"d", "color":"yellow"},
+    ];
 
+    $scope.dropper = function(event, dragData){
+        console.log("event ="+ event);
+        console.log("dragData ="+ dragData);
+    }
 });
