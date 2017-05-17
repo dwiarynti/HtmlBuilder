c4453ccdfccacad22fb1e78c17cf00eb35afd57d
Rjuzariah <riajuzariah@gmail.com>
2017-05-17 13:37:25 +0700

    delete git ignore

---
+++ public/javascripts/angular-assets/route.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module("RDash", [
+    "ui.bootstrap",
+    "ui.router",
+    "oc.lazyLoad",
+    "common.services", 
+    "dndLists"]);
+
+app.config([
+    '$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        
+        $stateProvider
+            .state('home', {
+                    url: '/home',
+                    templateUrl: '/javascripts/angular-assets/partialviews/home.html'
+            })
+            .state('profile', {
+                    url: '/home',
+                    templateUrl: '/javascripts/angular-assets/partialviews/home.html'
+            });
+    }
+
+]);
